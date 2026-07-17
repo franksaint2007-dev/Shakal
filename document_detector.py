36efb92f07eab365ad5fef25eb376c4743389e73
franksaint2007-dev <franksaint2007@gmail.com>
2026-07-18 00:30:18 +0200

    Create document_detector.py

---
+++ document_detector.py
@@ -0,0 +1,12 @@
+class DocumentDetector:
+
+
+    def scan(self,document):
+
+        return {
+            "fraud_probability":0.05,
+            "verified":True
+        }
+
+
+detector=DocumentDetector()
